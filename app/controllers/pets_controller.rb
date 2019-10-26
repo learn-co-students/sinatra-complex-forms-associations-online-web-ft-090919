ca8f5055380af829853e45b9292a55d8d304265a
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-25 22:44:20 -0400

    Feat working app, passing all tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,44 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.find_or_create_by(name: params[:pet][:name])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    elsif !params[:pet][:owner_id].empty?
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
-    @pet = Pet.find(params[:id])
+  get '/pets/:id' do
+    @pet = Pet.find_by_id(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
+  patch '/pets/:id' do
+    params.delete("_method")
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params[:new_owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:new_owner][:name])
+    elsif !params[:owner][:name].empty?
+      @pet.owner = Owner.find_by(params[:owner])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
